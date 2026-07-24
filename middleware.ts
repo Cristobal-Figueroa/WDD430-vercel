80c5a441070e74b714f975852f67fe9309d966dd
cristobal-figueroa <cristobalfigueroa92@gmail.com>
2026-07-23 20:15:12 -0400

    cambios

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,19 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import { NextResponse } from 'next/server';
+import { auth } from './auth';
  
-export default NextAuth(authConfig).auth;
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnDashboard = req.nextUrl.pathname.startsWith('/dashboard');
+  if (isOnDashboard) {
+    if (isLoggedIn) return NextResponse.next();
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
+  } else if (isLoggedIn) {
+    return NextResponse.redirect(new URL('/dashboard', req.nextUrl));
+  }
+  return NextResponse.next();
+});
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
